4d77251a916e02059887e5abb23e9979f49c621c
JosDamQ <damian.garcia.12.2004@gmail.com>
2025-11-23 18:22:46 -0600

    feat(add): task structure module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { PrismaService } from './prisma/prisma.service';
 import { PrismaModule } from './prisma/prisma.module';
+import { TaskModule } from './task/task.module';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [PrismaModule, TaskModule],
   controllers: [],
   providers: [PrismaService],
 })
